39a4d597ee926edeef32bf326169922c3fcd3045
pabloh2001 <phoyos06@gmail.com>
2023-01-17 21:10:41 -0500

    actualizando js

--- static/js/employee.js
+++ static/js/employee.js
@@ -135,19 +135,3 @@ window.addEventListener('DOMContentLoaded', (e) => {
         elemento.max = now;
     });
 });
-
-
-var forms = document.querySelectorAll('.needs-validation')
-
-// Bucle sobre ellos y evitar el envío
-Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-    form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-    }, false)
-    })
